5b7d0462f24f0afde87f930ed5616c5a4ae6adfe
Chandrasiri <chandrasiris@gmail.com>
2022-06-28 06:49:21 +0530

    Initial

---
+++ WebApplication9/WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Faculty()
+        {
+            return View();
+        }
+        public IActionResult Facility()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FacilityPost()
+        {
+            return RedirectToAction("Facility");
+        }
+        public IActionResult BookNow()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BookNowPost()
+        {
+            return RedirectToAction("BookNow");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
